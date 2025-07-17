0cde0570923f43b16644035955290eacfb643bbe
Thefelixctm <166573515+Thefelixctm@users.noreply.github.com>
2025-07-17 01:37:39 -0400

    Update download_data.py

--- Proyecto-Final/download_data.py
+++ Proyecto-Final/download_data.py
@@ -1,7 +1,4 @@
 import os
-import gdown 
-
-import os
 import gdown
 
 def descargar_archivos():
@@ -9,19 +6,19 @@ def descargar_archivos():
 
     archivos = [
         {
-            "url": "https://drive.google.com/uc?id=1iJqgnN7y9_r82Wr-UT_efGhSFuIF0qWP",
+            "url": "https://drive.google.com/uc?id=1iJqgnN7y9_r82Wr-UT_efGhSFuLF0qWP",
             "nombre": "imdb_dataset.csv"
         },
         {
-            "url": "https://drive.google.com/uc?id=1RKrL7Bd_qyINZT0mAaCOiXNPShtxUl6Q",
+            "url": "https://drive.google.com/uc?id=1RKrL7Bd_qyINZT0mAaCOiXNPshtxUl6Q",
             "nombre": "imdb_episodios.csv"
         },
         {
-            "url": "https://drive.google.com/uc?id=1s3TkbU_wEUdhEf30nGDx_RZj1g5E0HgP",
+            "url": "https://drive.google.com/uc?id=1s3Tkbu_wEUdhEf30nGDx_RZj1g5E0HgP",
             "nombre": "title.basics.tsv"
         },
         {
-            "url": "https://drive.google.com/uc?id=102-W0uUUU7Y0O7UfXsj3zSnYhcahOAQA",
+            "url": "https://drive.google.com/uc?id=102-W0wUUJ7Y007UfXsJ3zSnYhcahQQAA",
             "nombre": "title.episode.tsv"
         },
         {
@@ -34,6 +31,6 @@ def descargar_archivos():
         destino = os.path.join("data", archivo["nombre"])
         if not os.path.exists(destino):
             print(f"Descargando {archivo['nombre']}...")
-            gdown.download(archivo["url"], destino, quiet=False)
+            gdown.download(archivo["url"], destino, quiet=False, fuzzy=True)
         else:
             print(f"{archivo['nombre']} ya existe.")
